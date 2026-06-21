89c0a43ce6f1178b971df4fbc5d84531d1596f59
Biagio Distefano <biagio@biagiodistefano.io>
2026-06-21 14:24:05 +0200

    FEATURE_GOOGLE_SSO default to False

--- src/revel/settings/features.py
+++ src/revel/settings/features.py
@@ -6,7 +6,7 @@ from decouple import config
 # their domain settings module, vs. runtime SiteSettings in the DB).
 
 FEATURE_LLM_EVALUATION: bool = config("FEATURE_LLM_EVALUATION", default=True, cast=bool)
-FEATURE_GOOGLE_SSO: bool = config("FEATURE_GOOGLE_SSO", default=True, cast=bool)
+FEATURE_GOOGLE_SSO: bool = config("FEATURE_GOOGLE_SSO", default=False, cast=bool)
 FEATURE_MALWARE_SCAN: bool = config("FEATURE_MALWARE_SCAN", default=True, cast=bool)
 FEATURE_TELEGRAM: bool = config("FEATURE_TELEGRAM", default=True, cast=bool)
 FEATURE_ORGANIZATION_CREATION: bool = config("FEATURE_ORGANIZATION_CREATION", default=True, cast=bool)
